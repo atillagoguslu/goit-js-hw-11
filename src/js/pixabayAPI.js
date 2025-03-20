b53a7d8ea2e084297322eaa4218efdfae91e1dd7
Atilla.G <atillagoguslu@hotmail.com>
2025-03-21 00:27:57 +0300

    Add loader component and integrate iziToast for error handling in image search; update gallery styles

--- src/js/pixabayAPI.js
+++ src/js/pixabayAPI.js
@@ -1,11 +1,21 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 const searchText = document.querySelector('[data-search-input]');
 const searchButton = document.querySelector('[data-search-button]');
 
 const API_KEY = '49441477-341b75558156795e6f3713ba3';
 const BASE_URL = 'https://pixabay.com/api/';
 
-console.log('API_KEY:', API_KEY);
-console.log('BASE_URL:', BASE_URL);
+const iziToastSettings = {
+  position: 'topCenter',
+  timeout: 3000,
+  progressBarColor: 'red',
+  transitionIn: 'fadeInDown',
+  transitionOut: 'fadeOutUp',
+};
 
 let fetchedData = [];
 
@@ -33,12 +43,38 @@ const fetchImages = async (query, page = 1) => {
 // Search button event listener and handler -------------------
 const handleSearch = async event => {
   event.preventDefault();
-  console.log('handleSearch function called');
+  gallerySection.innerHTML = `
+  <div class="loader">
+    <p>Loading...</p>
+  </div>
+  `;
+  // Loader text
+  const loader = document.querySelector('.loader');
+  if (loader) {
+    loader.style.display = 'flex';
+  }
+  // -----------------
+
   const query = searchText.value;
-  const images = await fetchImages(query);
-  fetchedData = images;
-  console.log(images[0]);
-  setGallery(images);
+  try {
+    const images = await fetchImages(query);
+    if (images.length === 0) {
+      iziToast.show({
+        ...iziToastSettings,
+        title: 'Error',
+        message: `No images found with "${query}" query.`,
+        color: 'red',
+      });
+      return;
+    }
+    fetchedData = images;
+    console.log(images[0]);
+    // setGallery(images);
+  } finally {
+    if (loader) {
+      // loader.style.display = 'none';
+    }
+  }
 };
 searchButton.addEventListener('click', handleSearch);
 // ------------------------------------------------------------
@@ -46,11 +82,18 @@ searchButton.addEventListener('click', handleSearch);
 // Gallery section
 const gallerySection = document.querySelector('.gallery-section');
 
+let lightbox = new SimpleLightbox('.gallery-section a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
 const createGalleryItem = image => {
   const galleryItem = document.createElement('div');
   galleryItem.classList.add('gallery-item');
   galleryItem.innerHTML = `
-    <img width="${image.webformatWidth}" height="${image.webformatHeight}" src="${image.webformatURL}" alt="${image.tags}" />
+    <a href="${image.largeImageURL}">
+      <img width="${image.webformatWidth}" height="${image.webformatHeight}" src="${image.webformatURL}" alt="${image.tags}" />
+    </a>
     <div class="info">
       <div class="info-box">
         <b>Likes</b>
@@ -79,4 +122,5 @@ const setGallery = images => {
     const galleryItem = createGalleryItem(image);
     gallerySection.appendChild(galleryItem);
   });
+  lightbox.refresh();
 };
